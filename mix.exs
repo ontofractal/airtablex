d280ef573cdd135e9979d550a52652d9c3c8525b
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-03-20 15:38:42 +0200

    update deps

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Airtable.Mixfile do
       {:mix_test_watch, ">= 0.0.0", only: [:dev, :test]},
       {:power_assert, ">= 0.0.0"},
       {:exconstructor, ">= 1.0.0"},
-      {:poison, "~> 1.5"}
+      {:poison, "~> 2.2"}
     ]
   end
 end
