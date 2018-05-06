2639538004194e6767e0a8fd604fb1936b391872
ontofractal <ontofractal@protonmail.com>
2018-05-06 15:30:32 +0100

    fix breaking of backwards compat, add record struct

--- lib/airtable.ex
+++ lib/airtable.ex
@@ -1,5 +1,6 @@
 defmodule Airtable do
   use Tesla
+  alias Airtable.Record
   @api_key Application.get_env(:airtable, :api_key)
   @base Application.get_env(:airtable, :base)
 
@@ -17,13 +18,27 @@ defmodule Airtable do
     case get!(new(), table, query: [offset: offset]) do
       %{status: 200, body: %{"records" => records, "offset" => offset}} ->
         {:ok, next_offset_records} = fetch_records(table, %{offset: offset})
-        {:ok, records ++ next_offset_records}
+        {:ok, Enum.map(records, &Record.new/1) ++ next_offset_records}
 
       %{status: 200, body: %{"records" => records}} ->
-        {:ok, records}
+        {:ok, Enum.map(records, &Record.new/1)}
 
       env ->
         {:error, env}
     end
+  end
+
+  def fetch_record(table, id, opts \\ %{}) do
+    case get(new(), "#{table}/#{id}") do
+      %{status: 200, body: record} -> {:ok, Record.new(record)}
+      %{status: 404} -> {:error, :not_found}
+      env -> {:error, env}
+    end
+  end
+
+  defmodule Record do
+    defstruct [:id, :fields, :created_time]
+
+    use ExConstructor
   end
 end
